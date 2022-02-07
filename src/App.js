d79a28709a959cd243b8e576c57ddeaac5f22158
jheeju <jheeju.dev@gmail.com>
2022-02-07 16:56:38 +0900

    feat: basic styledButton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+`;
+
 function App() {
-  return <div className="App">React Style-components Tutorial</div>;
+  return (
+    <div className="App">
+      <button>Button</button>
+      <StyledButton>Styled Button</StyledButton>
+    </div>
+  );
 }
 
 export default App;

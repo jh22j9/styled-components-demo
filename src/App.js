3e78cb936da5c5d34d857d447369855d189a3337
jheeju <jheeju.dev@gmail.com>
2022-02-07 17:22:39 +0900

    feat: using props

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import StyledButton from "./components/Button/Button";
 function App() {
   return (
     <div className="App">
-      <button>Button</button>
       <StyledButton>Styled Button</StyledButton>
+      <div>
+        <br />
+      </div>
+      <StyledButton variant="outline">Styled Button</StyledButton>
     </div>
   );
 }
--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
   border: 2px solid #4caf50;
-  background-color: #4caf50;
-  color: white;
+  background-color: ${(props) =>
+    props.variant === "outline" ? "#FFF" : "#4caf50"};
+  color: ${(props) => (props.variant === "outline" ? "#4caf50" : "#FFF")};
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;

65e71f4ec3f0de29530c86a2c20ca307497769be
jheeju <jheeju.dev@gmail.com>
2022-02-08 10:08:55 +0900

    feat: theming

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
+import { ThemeProvider } from "styled-components";
 import logo from "./logo.svg";
 import StyledButton, {
   FancyButton,
   SubmitButton,
+  DarkButton,
 } from "./components/Button/Button";
 import { AnimatedLogo } from "./components/Button/Button.style";
 
+const theme = {
+  dark: {
+    primary: "#000",
+    text: "#fff",
+  },
+  light: {
+    primary: "#fff",
+    text: "#000",
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <AnimatedLogo src={logo} />
-      <StyledButton type="submit">Styled Button</StyledButton>
-      <div>
-        <br />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AnimatedLogo src={logo} />
+        <StyledButton type="submit">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant="outline">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <FancyButton as="a">Fancy Button</FancyButton>
+        {/* as - polymorphic prop */}
+        <div>
+          <br />
+        </div>
+        <SubmitButton>Submit</SubmitButton>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Dark Button</DarkButton>
       </div>
-      <StyledButton variant="outline">Styled Button</StyledButton>
-      <div>
-        <br />
-      </div>
-      <FancyButton as="a">Fancy Button</FancyButton>
-      {/* as - polymorphic prop */}
-      <div>
-        <br />
-      </div>
-      <SubmitButton>Submit</SubmitButton>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,9 @@
-import { StyledButton, FancyButton, SubmitButton } from "./Button.style";
+import {
+  StyledButton,
+  FancyButton,
+  SubmitButton,
+  DarkButton,
+} from "./Button.style";
 
 export default StyledButton;
-export { FancyButton, SubmitButton };
+export { FancyButton, SubmitButton, DarkButton };
